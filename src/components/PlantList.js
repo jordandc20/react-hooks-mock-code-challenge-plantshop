16f7030566746bf60d879e7f1ec1ac4e445a84ba
Diana Jordan <dianacjordan@gmail.com>
2023-06-06 20:48:12 -0400

    finish

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,16 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({plantList}) {
+
+const plant = plantList.map(plant=>(
+  <PlantCard key={plant.id} plant={plant} />
+))
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {plant}
+      </ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plantList, setPlantList] = useState([])
+  const[filterString,setFilterString] = useState('')
+
+  useEffect(() => {
+    fetch(' http://localhost:6001/plants')
+      .then(resp => resp.json())
+      .then(data => setPlantList(data))
+  }, [])
+
+const handleNewPlant = (newPlant)=>{
+setPlantList(plantList.concat(newPlant))
+}
+
+const visiblePlants = plantList.filter(plant => plant.name.toLowerCase().includes(filterString.toLowerCase()))
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onNewPlant={handleNewPlant}/>
+      <Search onFilter={setFilterString} filterString={filterString}/>
+      <PlantList plantList={visiblePlants}/>
     </main>
   );
 }
